8c898a3fca1ef7801c6870a92fc199846c1569cf
Patrik Ángyán <patesz1999@gmail.com>
2023-11-17 16:20:11 +0100

    pause, basic Tetromino placing

--- src/main/kotlin/com/tetris_time/Game.kt
+++ src/main/kotlin/com/tetris_time/Game.kt
@@ -2,7 +2,8 @@ package com.tetris_time
 
 import com.tetris_time.enums.MoveDirection
 import com.tetris_time.renderables.Map
-import com.tetris_time.renderables.tetrominos.IBlock
+import com.tetris_time.renderables.Tetromino
+import com.tetris_time.renderables.tetrominos.*
 import javafx.animation.AnimationTimer
 import javafx.application.Application
 import javafx.event.EventHandler
@@ -12,6 +13,7 @@ import javafx.scene.canvas.Canvas
 import javafx.scene.canvas.GraphicsContext
 import javafx.scene.input.KeyCode
 import javafx.stage.Stage
+import kotlin.Exception
 
 class Game : Application() {
 
@@ -26,11 +28,18 @@ class Game : Application() {
     // TODO load images
     // private lateinit var space: Image
 
-    private val currentTetromino = IBlock() // TODO getNextTetromino() { create random }
+    private var currentTetromino: Tetromino = IBlock() // TODO getNextTetromino() { create random }
+
+    // TODO rows of Fields instead
+    private val placedTetrominos: MutableList<Tetromino> = mutableListOf()
 
     private var lastSystemUpdateTime: Long = System.nanoTime()
 
+    private val allowedDirectionKeys = listOf(KeyCode.LEFT, KeyCode.RIGHT, KeyCode.DOWN)
+
     private var latestKey: KeyCode? = null
+
+    private var paused: Boolean = false
 
     override fun start(mainStage: Stage) {
         mainStage.title = "Tetris Time"
@@ -60,7 +69,9 @@ class Game : Application() {
 
     private fun prepareActionHandlers() {
         mainScene.onKeyPressed = EventHandler { event ->
-            latestKey = event.code
+            if (listOf(*allowedDirectionKeys.toTypedArray(), KeyCode.ESCAPE).contains(event.code)) {
+                latestKey = event.code
+            }
         }
     }
 
@@ -78,8 +89,8 @@ class Game : Application() {
 
         // perform world updates
         performInputUpdate()
-        if (elapsedNanos > 1_000_000_000) {
-            currentTetromino.move(MoveDirection.DOWN)
+        if (elapsedNanos > 1_000_000_000 && !paused) {
+            moveOrPlaceTetromino(MoveDirection.DOWN)
             lastSystemUpdateTime = currentNanoTime
         }
 
@@ -90,7 +101,7 @@ class Game : Application() {
         Map().render(graphicsContext.canvas)
 
         // draw rows
-        // TODO
+        placedTetrominos.forEach{ it.render(graphicsContext.canvas) }
 
         // draw current Tetromino
         currentTetromino.render(graphicsContext.canvas)
@@ -101,16 +112,28 @@ class Game : Application() {
             return
         }
 
-        if (latestKey == KeyCode.LEFT) {
-            currentTetromino.move(MoveDirection.LEFT)
-        }
-        if (latestKey == KeyCode.RIGHT) {
-            currentTetromino.move(MoveDirection.RIGHT)
-        }
-        if (latestKey == KeyCode.DOWN) {
-            currentTetromino.move(MoveDirection.DOWN)
+        if (latestKey == KeyCode.ESCAPE) {
+            paused = !paused
+        } else if(!paused) {
+            moveOrPlaceTetromino(
+                when (latestKey) {
+                    KeyCode.LEFT -> MoveDirection.LEFT
+                    KeyCode.RIGHT -> MoveDirection.RIGHT
+                    KeyCode.DOWN -> MoveDirection.DOWN
+                    else -> throw Exception("Direction not allowed!")
+                }
+            )
         }
 
         latestKey = null
+    }
+
+    private fun moveOrPlaceTetromino(direction: MoveDirection) {
+        try {
+            currentTetromino.move(direction)
+        } catch (e: Exception) {
+            placedTetrominos.add(currentTetromino)
+            currentTetromino = TBlock()
+        }
     }
 }
